61b0171a5d43e789783579ab7e35115ab15faa71
IRjSI <rajusuthar0077@gmail.com>
2025-04-02 16:25:37 +0530

    Add JWT authentication and middleware for HTTP and WebSocket servers

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,5 +1,41 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import middleware from "./middleware";
 
 const app = express();
 
-app.listen(3000);
+//@ts-ignore
+app.post('/signup', (req,res) => {
+    try {
+        const { name, email, password } = req.body;
+        const existing = ""
+        if (existing) {
+            return res.json({
+                message: 'User already exists'
+            })
+        }
+        res.json({
+            message: ''
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.post('/signin', (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+app.post('/room', middleware, (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+app.listen(3001);
